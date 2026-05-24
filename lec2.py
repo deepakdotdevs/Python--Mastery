fe96cb3bc273d4859b6f3f7b6397a151306e70f0
deepakdotdevs <deepak.06934@gmail.com>
2026-05-24 17:06:09 +0530

    Added Python file

---
+++ lec2.py
@@ -0,0 +1,12 @@
+# first install pandas using pip install pandas, coz pandas is a external module
+
+import pandas
+import hashlib # this module is build-in module, so no need to install it in terminal
+import tensorflow # this module is a external module, so need to install using pip install tensorflow
+
+print("hi")
+
+
+
+
+
