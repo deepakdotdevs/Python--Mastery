9144c524c15a862905cce2af58134f3d1f6a149a
deepakdotdevs <deepak.06934@gmail.com>
2026-05-24 20:43:20 +0530

    Lecture 3

---
+++ lec3.py
@@ -0,0 +1,16 @@
+# print(7)
+# print("Hello World !!") # string is always in "--STRING--"
+
+# Code is executed Line by Line
+
+# print("Deepak")
+# print(500)
+# print("Bye-Bye")
+
+# Computation in Python 
+
+print(20+65)
+print(87*92)
+print(87/21)
+print(875%26)
+print(800-600)
