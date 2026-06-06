16b46a3674401f91c2f9dfb39031564771d79954
deepakdotdevs <deepak.06934@gmail.com>
2026-06-06 11:29:00 +0530

    Lecture No. - 29

---
+++ lec29.py
@@ -0,0 +1,29 @@
+# Key Points : Set (No repeated entries) 
+
+# ---------------------------------------------------------------------
+
+# Sets are unordered collection of data items. They
+# store multiple items in a single variable. Set items are
+# separated by commas and enclosed within curly
+# brackets {}. Sets are unchangeable, meaning you
+# cannot change items of the set once created. Sets do
+# not contain duplicate items.
+
+# ---------------------------------------------------------------------
+
+# info = {"Deepak", 19, False, 29.02, 19, 2054}
+# print(info)
+
+# ---------------------------------------------------------------------
+
+# empty_Set = {} # it's not a empty set, 
+# empty_Set1 = set() # this is the empty set
+# print(type(empty_Set)) # it's type of class is dictionary
+# print(type(empty_Set1))
+
+# ---------------------------------------------------------------------
+
+info = {"Deepak", 45, 45, 20.6, True, 'POP'}
+for i in info:
+    print(i)
+    
