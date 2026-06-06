60858bbe6ddc9158233861a07406aa623d206a06
deepakdotdevs <deepak.06934@gmail.com>
2026-06-06 10:18:06 +0530

    Lec 27 - PIP-8 & Zen of Python

--- lec27.py
+++ lec27.py
@@ -14,9 +14,37 @@
 
 # ----------------------------------------------------------------------
 
-def square(n):
-    '''Takes in a number n, returns the square of n''' # this is the docstring
-    print(n**2)
-square(5)
-# to print the docstring : means docstrings are not ignored as comments
-print(square.__doc__)
+# def square(n):
+#     '''Takes in a number n, returns the square of n''' # this is the docstring
+#     print(n**2)
+# square(5)
+# # to print the docstring : means docstrings are not ignored as comments
+# print(square.__doc__)
+
+# ----------------------------------------------------------------------
+
+# PIP 8 : 
+
+# PEP 8 is a document that provides guidelines and
+# best practices on how to write Python code. It was
+# written in 2001 by Guido van Rossum, Barry Warsaw,
+# and Nick Coghlan. The primary focus of PEP 8 is to
+# improve the readability and consistency of Python
+# code.
+
+# PEP stands for Python Enhancement Proposal, and
+# there are several of them. A PEP is a document that
+# describes new features proposed for Pythğn and
+# documents aspects of Python, like design and style,
+# for the community.
+
+# ----------------------------------------------------------------------
+
+# The Zen of Python : 
+
+# Long time Pythoneer Tim Peters succinctly channels
+# the BDFL's guiding principles for Python's design into
+# 20 aphorisms, only 19 of which have been written
+# down.
+
+import this # Prints a Poem known as Zen of Python
