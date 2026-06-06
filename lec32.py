a07fe1cfc3338b9ba8a13d6c12e66604c7afe79f
deepakdotdevs <deepak.06934@gmail.com>
2026-06-06 21:51:49 +0530

    Lecture No. 32

---
+++ lec32.py
@@ -0,0 +1,31 @@
+# Key Points : Dictionaries Methods :
+
+# ---------------------------------------------------------------------
+
+# update():
+
+# ep1 = {122:69, 157:85, 426:72, 586:56}
+# ep2 = {566:90, 396:98, 101:84, 103:99, 899:99.09}
+# ep1.update(ep2)
+# print(ep1)
+# emp = {} # this how we create empty dictionary
+# print(emp)
+# ep1.clear()
+# print(ep1) # emply coz of clear()
+
+# ep2.pop(566) # for remiving key - value pair from dict
+# print(ep2)
+
+# ep2.popitem() # remove last key-value pair
+# print(ep2)
+
+# # del keyword : delete complte dictionary :
+
+# del ep2[103] # delete only this specific key-value pair
+# print(ep2)
+
+# del ep2 # delete entire dict
+# print(ep2)
+
+# ---------------------------------------------------------------------
+
