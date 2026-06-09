efda5a67b2b33c0db2f2a84eecd92b501c690ae8
deepakdotdevs <deepak.06934@gmail.com>
2026-06-09 07:58:15 +0530

    Lecture No. - 36

---
+++ lec36.py
@@ -0,0 +1,15 @@
+# Key Points : Raising Customer errors in Python :
+
+# ---------------------------------------------------------------------
+
+# raise keyword is used : 
+
+# ---------------------------------------------------------------------
+
+a = int(input("Enter any value b/w 5 and 9 : "))
+if(a<5 or a>9):
+    raise ValueError("Value should be b/w 5 and 9")
+
+# and that's how we raise built-in errors in python using raise keyword in python
+
+# ---------------------------------------------------------------------
