13978d5e287735fc887c1a0699ae8c2983ce7e96
deepakdotdevs <deepak.06934@gmail.com>
2026-06-19 21:38:41 +0530

    Lecture No. 57

---
+++ lec57.py
@@ -0,0 +1,79 @@
+# Key Points : Getters and Setters
+
+# Getters in Python are methods that are used to access the values of an object's
+# properties. They are used to return the value of a specific property, and are
+# typically defined using the @property decorator. Here is an example of a simple
+# class with a getter method:
+
+# class MyClass:
+# def __ init __ (self, value):
+# self ._ value = value
+
+# @property
+# def value(self):
+# return self ._ value
+
+# In this example, the MyClass class has a single property, _value, which is
+# initialized in the init method. The value method is defined as a getter using the
+# @property decorator, and is used to return the value of the _value property.
+
+# To use the getter, we can create an instance of the MyClass class, and then
+# access the value property as if it were an attribute:
+
+# >>> obj = MyClass(10)
+# >>> obj.value
+# 10
+
+# -------------------------------------------------------
+
+# Program : 
+
+class MyClass:
+    def __init__(self, value):
+        self._value = value
+    def show(self):
+        print(f"Value is {self._value}")
+    # decorator
+    @property # it is works as a getter
+    def ten_value(self):
+        return 10*self._value
+    @ten_value.setter # setter
+    def ten_value(self, new_value):
+        self._value = new_value/10
+
+obj = MyClass(10)
+# i want direct value use like :
+print(obj.ten_value) # can do using this , but i don't want like this i use @property, like i want 10 times of that value and in that case @property is a getter
+obj.ten_value = 67 # this is setter
+obj.show()
+
+# Setters
+
+# It is important to note that the getters do not take any parameters and we cannot
+# set the value through getter method.For that we need setter method which can be
+# added by decorating method with @property_name.setter
+
+# Here is an example of a class with both getter and setter:
+
+# class MyClass:
+# def __ init __ (self, value):
+# self ._ value = value
+
+# @property
+# def value(self):
+# return self ._ value
+
+# @value.setter
+# def value(self, new_value):
+# self ._ value = new_value
+
+# We can use setter method like this:
+
+# >>> obj = MyClass(10)
+# >>> obj.value = 20
+# >>> obj.value
+# 20
+
+# In conclusion, getters are a convenient way to access the values of an object's
+# properties, while keeping the internal representation of the property hidden. This
+# can be useful for encapsulation and data validation.
